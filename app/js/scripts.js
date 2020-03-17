8bbdbd957f9437b62313cf25ba6e4b369ab90e47
SeryogaGitHub <main.email.seryoga@gmail.com>
2020-03-17 18:30:13 +0200

    start filter

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,8 +1,53 @@
 'use strict';
 
 $(document).ready(function() {
-  $('.dropdown').on("click", function () {
-    $(this).find('.dropdown-container').stop().slideToggle();
-    $(this).toggleClass('active');
+  let $specialtyList = $('#specialty-list');
+  let $dropdownContainer = $specialtyList.find('.dropdown-container');
+
+  let closeDropdown = () => {
+    $dropdownContainer.stop().slideUp();
+    $specialtyList.removeClass('active');
+  };
+
+  // відкриття dropdown
+  $('#specialty-list .toggle').on("click", function (e) {
+    $dropdownContainer.stop().slideToggle();
+    $specialtyList.toggleClass('active');
+  });
+
+  // змінана назви товара в кнопці
+  $('#specialty-list .item').on("click", function (e) {
+    let $this = $(this);
+    let text = $this.text();
+    let $dropdown = $(this).parents('#specialty-list');
+
+    closeDropdown();
+    $dropdown.find('.toggle').text(text);
+  });
+
+  // serch
+  $('#search-product').on("keyup", function (e) {
+    let val = $(this).val();
+    val = val.toLowerCase();
+
+    $('#specialty-list .item').each(function () {
+      let $this = $(this);
+      let text = $(this).text();
+      text = text.toLowerCase();
+
+      if(!text.match(val)){
+        $this.hide();
+      } else {
+        $this.show();
+      }
+    });
+  });
+
+  // закриття dropdown в не поля
+  $(document).mouseup(function (e){
+    if (!$specialtyList.is(e.target)
+      && $specialtyList.has(e.target).length === 0) {
+      closeDropdown();
+    }
   });
 });
